622420654e27c7e7f00fef47a7dea94fc4d0c75b
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-26 09:08:52 +0600

    dom recap

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Folder is working");
+document.getElementById("change-bg").addEventListener("click", function() {
+    let friends = document.getElementsByClassName("friend");
+    for (friend of friends) {
+        let element = friend;
+        element.style.backgroundColor = "pink";
+        element.style.padding = "10px";
+        element.style.margin = "10px";
+    }
+});
+
+document.getElementById("add-new").addEventListener("click", function() {
+    let parent = document.getElementById("friends");
+    let newItem = document.createElement("div");
+    newItem.innerHTML = `
+    <h2>Friend - 66</h2>
+    <p> Explicabo aspernatur eveniet, dolorum dignissimos error nesciunt veniam aperiam totam officia eius libero inventore ab velit. Sed aut sapiente natus.</p>
+`;
+    newItem.setAttribute("class", "friend");
+    parent.appendChild(newItem);
+});
