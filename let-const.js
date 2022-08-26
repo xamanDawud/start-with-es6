36056adaf6c3f81352f6ab5c70204109bbb8bb40
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-26 09:30:03 +0600

    Let and Const explored

---
+++ let-const.js
@@ -0,0 +1,15 @@
+// You can Reassign the value of let
+let name = "xaman";
+xaman = "Dawud";
+console.log(xaman);
+
+You cannot Reassign the Const value
+const home = "DorgaBahapur";
+home = "Brammanbaria";
+console.log(home);
+
+// you can add with const value . it's possible
+// let firstName = "Isra";
+// const lastName = "Choudhury";
+// let addLetAndConst = firstName + " " + lastName;
+// console.log(addLetAndConst);
